b1159c3bbff3405fc4a9e779c0748a19da244120
zhangyunping <yp000925@outlook.com>
2021-06-01 10:35:56 +0800

    add git file

--- hubconf.py
+++ hubconf.py
@@ -115,8 +115,8 @@ if __name__ == '__main__':
 
     imgs = ['data/images/zidane.jpg',  # filename
             'https://github.com/ultralytics/yolov5/releases/download/v1.0/zidane.jpg',  # URI
-            cv2.imread('data/images/bus.jpg')[:, :, ::-1],  # OpenCV
-            Image.open('data/images/bus.jpg'),  # PIL
+            cv2.imread('param_set/images/bus.jpg')[:, :, ::-1],  # OpenCV
+            Image.open('param_set/images/bus.jpg'),  # PIL
             np.zeros((320, 640, 3))]  # numpy
 
     results = model(imgs)  # batched inference
--- mytrain.sh
+++ mytrain.sh
@@ -3,11 +3,11 @@
 python train.py \
 --weights '' \
 --cfg 'yolov5s.yaml' \
---data 'data/holodata.yaml' \
+--data 'holodata.yaml' \
 --hyp 'data/hyp_self.yaml' \
---epochs 300 \
+--epochs 15 \
 --batch-size 16 \
 --img-size [512, 512] \
 --adam \
---name 'exp01' \
+--name 'exp00' \
 --save_period 50 \
