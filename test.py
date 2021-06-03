e4a7da331c2565c1613b2efab491a98834d19f86
zhangyunping <yp000925@outlook.com>
2021-06-03 16:51:20 +0800

    recover test.py

--- test.py
+++ test.py
@@ -140,8 +140,8 @@ def test(data,
         targets[:, 2:] *= torch.Tensor([width, height, width, height]).to(device)  # to pixels
         lb = [targets[targets[:, 0] == i, 1:] for i in range(nb)] if save_hybrid else []  # for autolabelling
         t = time_synchronized()
-        # out = non_max_suppression(out, conf_thres, iou_thres, labels=lb, multi_label=True, agnostic=single_cls)
-        out = post_nms(out,0.45)
+        out = non_max_suppression(out, conf_thres, iou_thres, labels=lb, multi_label=True, agnostic=single_cls)
+        # out = post_nms(out,0.45)
         # list of detections, on (n,6) tensor per image [xyxy, conf, cls]
         t1 += time_synchronized() - t
 
